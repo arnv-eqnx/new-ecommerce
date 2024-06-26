cb6d6b5c22b59c767450d84f7a4284f4e3f031e0
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-06-27 00:58:48 +0530

    library added

---
+++ src/app/login/page.js
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+
+    return(<>
+        <div className="flex w-full h-1/2 bg-white">
+            <div className="flex-end " ></div>
+            <div></div>
+        </div>
+    </>)
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+'use client'
+import { Alert } from '@material-tailwind/react'
 import Image from 'next/image'
 import Nav from './components/nav/Nav'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-24 py-8">
+    <main className="flex min-h-screen bg-black flex-col px-24 py-8">
       <Nav />
     </main>
   )
